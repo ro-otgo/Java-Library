228d2cae353b402ed3b2c842b236b599450f532a
ManceboJ <jmancebo10@gmail.com>
2023-05-07 21:16:54 +0200

    Actualización a los antiguos atributos de Libros

--- src/main/java/com/universidadeuropea/dao/LibrosDao.java
+++ src/main/java/com/universidadeuropea/dao/LibrosDao.java
@@ -13,12 +13,12 @@ public class LibrosDao extends Dao <Libros, Long> implements ILibrosDao{
 
 	@Override
 	protected String selectById() {
-		return "SELECT * FROM libros WHERE id =?";
+		return "SELECT * FROM libros WHERE id_libro =?";
 	}
 
 	@Override
 	protected String deleteById() {
-		return "DELETE FROM libros WHERE id =?";
+		return "DELETE FROM libros WHERE id_libro =?";
 	}
 	
 	@Override
@@ -29,8 +29,11 @@ public class LibrosDao extends Dao <Libros, Long> implements ILibrosDao{
 	@Override
 	protected Libros mapear(ResultSet rs) throws SQLException {
 		Libros libro = new Libros();
-		libro.setId(rs.getLong("id"));
 		libro.setIdLibro(rs.getLong("id_libro"));
+		libro.setTitulo(rs.getString("id"));
+		libro.setIsbn(rs.getLong("isbn"));
+		libro.setAutor(rs.getString("autor"));
+		libro.setBorrado(rs.getBoolean("borrado"));
 		return libro;
 	}
 
@@ -44,30 +47,36 @@ public class LibrosDao extends Dao <Libros, Long> implements ILibrosDao{
 	@Override
 	public Libros save(Libros objeto) throws SQLException {
 		obtenerConexionDB();
-		PreparedStatement ps =getConnection().prepareStatement("INSERT INTO libros (id_libro) VALUES(?)",  Statement.RETURN_GENERATED_KEYS);
-		ps.setLong(1, objeto.getIdLibro());
+		PreparedStatement ps =getConnection().prepareStatement("INSERT INTO libros (titulo,isbn,autor,borrado) VALUES(?,?,?,?)",  Statement.RETURN_GENERATED_KEYS);
+		ps.setString(1, objeto.getTitulo());
+		ps.setLong(2, objeto.getIsbn());
+		ps.setString(3, objeto.getAutor());
+		ps.setBoolean(4, objeto.getBorrado());
 		ps.executeUpdate();
 		ResultSet rs = ps.getGeneratedKeys();
 		long pk =0; 
 		while (rs.next()) {
 			pk = rs.getLong(1);
 		}
-		objeto.setId(pk);
+		objeto.setIdLibro(pk);
 		cerrarConexion();
 		return objeto;
 	}
 
 	@Override
 	public boolean delete(Libros objeto) throws Exception {
-		return super.deleteById(objeto.getId());
+		return super.deleteById(objeto.getIdLibro());
 	}
 
 	@Override
 	public Libros update(Libros objeto) throws Exception {
 		obtenerConexionDB();
-		PreparedStatement ps =getConnection().prepareStatement("UPDATE libros SET id_libro =? WHERE id=?");
-		ps.setLong(1, objeto.getIdLibro());;
-		ps.setLong(2, objeto.getId());
+		PreparedStatement ps =getConnection().prepareStatement("UPDATE libros SET titulo =? , isbn =? , autor =? , borrado =? WHERE id_libro=?");
+		ps.setString(1, objeto.getTitulo());
+		ps.setLong(2, objeto.getIsbn());
+		ps.setString(3, objeto.getAutor());
+		ps.setBoolean(4, objeto.getBorrado());
+		ps.setLong(5, objeto.getIdLibro());
 		ps.executeUpdate();
 		cerrarConexion();
 		return objeto;
